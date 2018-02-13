38f2596e949fd5f3659f2dbcefa494465860d9af
Geoffroy Couprie <geo.couprie@gmail.com>
2018-02-13 18:15:34 +0100

    move the producer to a helper function

--- src/kafka.rs
+++ src/kafka.rs
@@ -1,5 +1,7 @@
 use futures::Stream;
 use futures::Future;
+use futures::future::lazy;
+
 use futures_cpupool::Builder;
 use tokio_core::reactor::Core;
 use warp10;
@@ -22,7 +24,8 @@ use warp10_post;
 use BufferedDomainTestResult;
 use RequestBenchEvent;
 
-fn expensive_computation(msg: OwnedMessage, warp10_url: &str, warp10_token: &str) -> String {
+//FIXME: send back an error
+fn expensive_computation(msg: OwnedMessage, warp10_url: &str, warp10_token: &str) {
     info!("Starting expensive computation on message");
     thread::sleep(Duration::from_millis(5000));
     info!("Expensive computation completed");
@@ -44,8 +47,6 @@ fn expensive_computation(msg: OwnedMessage, warp10_url: &str, warp10_token: &str
       let res = warp10_post(data, warp10_url.to_string(), warp10_token.to_string());
       println!("{:#?}", res);
     }
-
-    String::new()
 }
 
 // Creates all the resources and runs the event loop. The event loop will:
@@ -78,12 +79,6 @@ fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_
 
     consumer.subscribe(&[input_topic]).expect("Can't subscribe to specified topic");
 
-    // Create the `FutureProducer` to produce asynchronously.
-    let producer = ClientConfig::new()
-        .set("bootstrap.servers", brokers)
-        .set("produce.offset.report", "true")
-        .create::<FutureProducer<_>>()
-        .expect("Producer creation error");
 
     // Create a handle to the core, that will be used to provide additional asynchronous work
     // to the event loop.
@@ -99,29 +94,21 @@ fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_
                     None
                 }
             }
-        }).for_each(|msg| {     // Process each message
+        }).for_each(move |msg| {     // Process each message
             info!("Enqueuing message for computation");
-            let producer = producer.clone();
-            let topic_name = output_topic.to_owned();
             let owned_message = msg.detach();
 
             let u = url.clone();
             let t = token.clone();
             // Create the inner pipeline, that represents the processing of a single event.
-            let process_message = cpu_pool.spawn_fn(move || {
+            let process_message = cpu_pool.spawn(lazy(move || {
+                let url = u.clone();
+                let token = t.clone();
                 // Take ownership of the message, and runs an expensive computation on it,
                 // using one of the threads of the `cpu_pool`.
-                Ok(expensive_computation(owned_message, &u, &t))
-            }).and_then(move |computation_result| {
-                // Send the result of the computation to Kafka, asynchronously.
-                info!("Sending result");
-                producer.send_copy::<String, ()>(&topic_name, None, Some(&computation_result), None, None, 1000)
-            }).and_then(|d_report| {
-                // Once the message has been produced, print the delivery report and terminate
-                // the pipeline.
-                info!("Delivery report for result: {:?}", d_report);
-                Ok(())
-            }).or_else(|err| {
+                expensive_computation(owned_message, &u, &t);
+                Ok::<_, ()>(())
+            })).or_else(|err| {
                 // In case of error, this closure will be executed instead.
                 warn!("Error while processing message: {:?}", err);
                 Ok(())
@@ -135,5 +122,43 @@ fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_
     // Runs the event pool until the consumer terminates.
     core.run(processed_stream).unwrap();
     info!("Stream processing terminated");
+}
+
+fn send_message(brokers: &str, output_topic: &str, test_url: &str) {
+    // Create the event loop. The event loop will run on a single thread and drive the pipeline.
+    let mut core = Core::new().unwrap();
+
+    // Create the CPU pool, for CPU-intensive message processing.
+    //let cpu_pool = Builder::new().pool_size(4).create();
+    // Create the `FutureProducer` to produce asynchronously.
+    let producer = ClientConfig::new()
+        .set("bootstrap.servers", brokers)
+        .set("produce.offset.report", "true")
+        .create::<FutureProducer<_>>()
+        .expect("Producer creation error");
+
+    let handle = core.handle();
+    let topic_name = output_topic.to_string();
+    // Create the inner pipeline, that represents the processing of a single event.
+    /*let send = cpu_pool.spawn_fn(move || {
+      info!("Sending result");
+      let result = String::from("a");
+      let topic = topic_name.clone();
+      producer.send_copy::<String, ()>(&topic_name, None, Some(&result), None, None, 1000)
+    })*/
+    let result = String::from("a");
+    producer.send_copy::<String, ()>(&topic_name, None, Some(&result), None, None, 1000)
+    .and_then(|d_report| {
+      // Once the message has been produced, print the delivery report and terminate
+      // the pipeline.
+      info!("Delivery report for result: {:?}", d_report);
+      Ok(())
+    }).or_else(|err| {
+      // In case of error, this closure will be executed instead.
+      warn!("Error while processing message: {:?}", err);
+      Ok::<_, ()>(())
+    });
+    // Spawns the inner pipeline in the same event pool.
+    //handle.spawn(send);
 }
 
